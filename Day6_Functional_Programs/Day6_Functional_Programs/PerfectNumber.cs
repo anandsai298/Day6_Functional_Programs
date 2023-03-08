096e71157342a2716f15092c32bb38cf85d0b4ef
anandsaikumar <anandsai298@gmail.com>
2023-03-08 16:08:06 +0530

    [sai] Add : Is number is a Perfect number are not

---
+++ Day6_Functional_Programs/Day6_Functional_Programs/PerfectNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Functional_Programs
+{
+    public class PerfectNumber
+    {
+        public void PerNo(int numb)
+        {
+            int Res = 0;
+            for (int i=1;i<numb;i++)
+            {
+                if (numb % i == 0)
+                    Res += i;
+            }
+            if (Res == numb)
+                Console.WriteLine(Res + " is a Perfect Number");
+            else
+                Console.WriteLine(Res + " is Not a Perfect Number");
+        }
+    }
+}
--- Day6_Functional_Programs/Day6_Functional_Programs/Program.cs
+++ Day6_Functional_Programs/Day6_Functional_Programs/Program.cs
@@ -11,20 +11,27 @@ namespace Day5_Functional_Programs
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine("1.Febinocci_Series \n2.Reverse_Number\n3.Exit");
+                Console.WriteLine("1.Febinocci_Series \n2.Reverse_Number\n3.PerfectNumber\n4.Exit");
                 Console.WriteLine("Enter your option to execute logic ");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
                     case 1:
                         Febinocci_Series Fs = new Febinocci_Series();
+                        Console.WriteLine("print Febinocci series");
                         Fs.Febinocci(5);
                         break;
                     case 2:
                         Reverse_Number rv = new Reverse_Number();
+                        Console.WriteLine("print reverse number");
                         rv.Rev(12324);
                         break;
                     case 3:
+                        PerfectNumber pn = new PerfectNumber();
+                        Console.WriteLine("print perfect number or not");
+                        pn.PerNo(28);
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
