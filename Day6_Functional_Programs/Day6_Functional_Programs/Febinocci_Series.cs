8d62aed0ff45980cc6b509151807f1d7e1f1d042
anandsaikumar <anandsai298@gmail.com>
2023-03-08 15:40:27 +0530

    [sai] Add : Febinocci series is created

---
+++ Day6_Functional_Programs/Day6_Functional_Programs/Febinocci_Series.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Functional_Programs
+{
+    public class Febinocci_Series
+    {
+        public void Febinocci(int num)
+        {
+            int Fn = 0, Sn = 1, Tn = 0;
+            Console.WriteLine("first no --> "+Fn + "\n" +"second no --> "+ Sn);
+            for (int i=0;i<num;i++)
+            {
+                Tn = Fn + Sn;
+                Console.WriteLine("Febonocci series of given" + num + "is : " + Tn);
+                Fn = Sn;
+                Sn = Tn;
+            }
+        }
+    }
+}
---
+++ Day6_Functional_Programs/Day6_Functional_Programs/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day6_Functional_Programs;
+using System;
+namespace Day5_Functional_Programs
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Day6_Functional_Programs");
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("1.Febinocci_Series \n2.Exit");
+                Console.WriteLine("Enter your option to execute logic ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Febinocci_Series Fs = new Febinocci_Series();
+                        Fs.Febinocci(5);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
